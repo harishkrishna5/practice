1ae42c1f37b626f6a3641af43632f6309f6f9d31
harishkrishna5 <harishkrish09@gmail.com>
2024-08-24 18:20:32 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,21 @@
+/*
+ * Write a program to Subtract two numbers in java
+ */
+public class SubtractTwoNumbersInJava {
+    public static void main(String[] args) {
+           int firstNumber = 5;
+           int secondNumber = 2;
+ 
+           System.out.println("firstNumber - secondNumber = "
+                        + (firstNumber - secondNumber));
+ 
+    }
+}
+ 
+ 
+/*OUTPUT
+ 
+firstNumber - secondNumber = 3
+ 
+*/
+ 
